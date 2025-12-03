9ad2710b24a9e80481e1f07a7dcc1ab816e7a1e5
Tanujashree-H-V <tanujatanu354@gmail.com>
2025-12-03 14:38:47 +0530

    function in js

---
+++ function.js
@@ -0,0 +1,12 @@
+function sum(a,b){
+    sum=a+b;
+    console.log(sum);
+}
+
+function dsum(data){
+    console.log("the sum of the two no."+data);
+}
+function dsump(data){
+    console.log("sum was "+data);
+}
+sum(1,2);
