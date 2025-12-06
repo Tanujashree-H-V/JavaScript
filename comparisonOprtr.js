697fb544bdfabf242b5bf9bebab2adf52d41e067
Tanujashree-H-V <tanujatanu354@gmail.com>
2025-12-06 23:00:06 +0530

    comparison operator in js(equality and inequality oprtr)

---
+++ comparisonOprtr.js
@@ -0,0 +1,35 @@
+//Equality Operator (==)
+// Illustration of (==) operator
+let x = 5;
+let y = '5';
+
+// Checking of operands
+console.log(x == 5);
+console.log(y == 5);        
+console.log(x == y);
+
+console.log();
+
+// Check against special values
+console.log(NaN == NaN);
+console.log(0 == false);   
+console.log(0 == null);
+
+console.log();
+
+//Inequality Operator (!=)
+// Illustration of (!=) operator
+let a = 5;
+let b = '5';
+
+// Checking of operands
+console.log(a != 6);
+console.log(b != '5');        
+console.log(a != y);
+
+console.log();
+
+// Check against special values
+console.log(0 != false);   
+console.log(0 != null);
+console.log(NaN != NaN);
