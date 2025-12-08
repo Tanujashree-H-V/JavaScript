925847a3489db67ea9a474ebf2c216c924a04867
Tanujashree-H-V <tanujatanu354@gmail.com>
2025-12-08 22:38:09 +0530

    switch statement in js

---
+++ switchStmnt.js
@@ -0,0 +1,40 @@
+// User function Template for javascript
+
+const number=3;
+        // write your switch statement here
+if (number==0){
+console.log("Unknown")
+}
+else if (number<=9){
+    switch(true){
+            case number==1:
+                console.log("One")
+                break;
+            case number==2:
+                console.log("Two")
+                break;
+            case number==3:
+                console.log("Three")
+                break;
+            case number==4:
+                console.log("Four")
+                break;
+            case number==5:
+                console.log("Five")
+                break;
+            case number==6:
+                console.log("Six")
+                break;
+            case number==7:
+                console.log("Seven")
+                break;
+            case number==8:
+                console.log("Eight")
+                break;
+            case number==9:
+               console.log("Nine")
+                break;
+        }
+        }else{
+            console.log("Unknown")
+        }
